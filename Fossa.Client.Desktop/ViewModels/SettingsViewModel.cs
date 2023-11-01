ff5781c49c2c5b45ec089b48eac3e1ec138027df
pitzzahh <pitzzahh@gmail.com>
2023-11-01 12:07:44 +0800

    :sparkles: Add SettingsViewModel update logic

--- Fossa.Client.Desktop/ViewModels/SettingsViewModel.cs
+++ Fossa.Client.Desktop/ViewModels/SettingsViewModel.cs
@@ -20,11 +20,60 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+using System;
+using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using FluentAvalonia.UI.Controls;
+using Fossa.Client.Desktop.Services;
+using Fossa.Client.Desktop.Services.Updater;
 
 namespace Fossa.Client.Desktop.ViewModels;
 
-public class SettingsViewModel : ObservableObject
+// TODO: move update functionality to about page
+public partial class SettingsViewModel : ObservableObject
 {
-    
+    private readonly GithubUpdater _githubUpdater;
+    private readonly DialogFactory _dialogFactory;
+    [ObservableProperty] private double _downloadProgress;
+    [ObservableProperty] private bool _isProcessing;
+    [ObservableProperty] private string _version;
+    [ObservableProperty] private bool _isCheckingForUpdates;
+
+    public SettingsViewModel(GithubUpdater githubUpdater, DialogFactory dialogFactory)
+    {
+        _githubUpdater = githubUpdater;
+        _dialogFactory = dialogFactory;
+        Version = GithubUpdater.ReadCurrentVersion()?.ToString() ?? "Cannot read version";
+    }
+
+    [RelayCommand]
+    private async Task CheckForUpdates()
+    {
+        IsProcessing = true;
+        IsCheckingForUpdates = true;
+        try
+        {
+            var updateStatus = await _githubUpdater.CheckForUpdates();
+            IsCheckingForUpdates = false;
+            if (updateStatus != UpdateStatus.Available)
+            {
+                await _dialogFactory.CreateInfoDialog("No Update", "No updates available");
+                return;
+            }
+
+            var installUpdateDialog = await _dialogFactory.CreateInstallUpdateDialog("Update Available",
+                "An update is available, would you like to install it?");
+            if (installUpdateDialog is not ContentDialogResult.Primary) return;
+            await _githubUpdater.PerformUpdate(new Progress<double>(val => DownloadProgress = val * 100),
+                async () => await _dialogFactory.CreateInstallUpdateDialog("Update Downloaded",
+                    "Update has been downloaded, restart the application?") is ContentDialogResult.Primary
+            );
+        }
+        finally
+        {
+            IsProcessing = false;
+            IsCheckingForUpdates = false;
+        }
+    }
 }
